07e94f4ef90192278f94dd464962266fb49b3dc2
DmitriiK1 <dimonovec@mail.ru>
2020-09-07 18:04:28 +0300

    Доработал стили header в article

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,8 @@
 
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__menu');
+const headerButton = document.querySelector('.header__button');
+const circlePreloaderPlace = document.querySelector('.circle-preloader_place');
 
 function openMenu() {
   burger.classList.toggle('active');
@@ -10,4 +12,11 @@ function openMenu() {
 
 burger.addEventListener('click', () => {
   openMenu();
+});
+
+headerButton.addEventListener('click', () => {
+  circlePreloaderPlace.classList.add('circle-preloader');
+  setTimeout(() => {
+    circlePreloaderPlace.classList.remove('circle-preloader');
+  }, 1500);
 });
