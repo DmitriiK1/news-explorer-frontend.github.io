e6641839e4d158b595edb9569b16009abdf47b70
DmitriiK1 <dimonovec@mail.ru>
2020-09-16 16:52:48 +0300

    Исправил открытие popup-create popup-authorization

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,28 @@ const menu = document.querySelector('.header__menu');
 const headerButton = document.querySelector('.header__button');
 const circlePreloaderPlace = document.querySelector('.circle-preloader_place');
 
+const headerLinkRounded = document.querySelector('.header__link-rounded');
+const popup = document.querySelector('.popup');
+const closePopups = document.querySelectorAll('.popup__close');
+const enterAuthorization = document.querySelector('.popup__enter-authorization');
+const popupAuthorization = document.querySelector('.popup-authorization');
+
+headerLinkRounded.addEventListener('click', () => {
+  popup.classList.add('popup_is-opened');
+});
+closePopups.forEach((closePopup) => {
+  closePopup.addEventListener('click', () => {
+    popup.classList.remove('popup_is-opened');
+    popupAuthorization.classList.remove('popup_is-opened');
+  });
+});
+
+enterAuthorization.addEventListener('click', (event) => {
+  event.preventDefault();
+  popupAuthorization.classList.add('popup_is-opened');
+  popup.classList.remove('popup_is-opened');
+});
+
 function openMenu() {
   burger.classList.toggle('active');
   menu.classList.toggle('active');
