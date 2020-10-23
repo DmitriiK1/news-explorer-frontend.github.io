58d4dc6bd241ba75e0f9171e1ef669df9b500f01
DmitriiK1 <dimonovec@mail.ru>
2020-10-23 12:14:52 +0300

    переключение попапов

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,6 +10,42 @@ const popup = document.querySelector('.popup');
 const closePopups = document.querySelectorAll('.popup__close');
 const enterAuthorization = document.querySelector('.popup__enter-authorization');
 const popupAuthorization = document.querySelector('.popup-authorization');
+const signupForm = document.querySelector('.popup-authorization .popup__form');
+
+const API_URL = 'http://localhost:3001';
+
+signupForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const name = event.target.querySelector('input[name="name"]').value;
+  const email = event.target.querySelector('input[name="email"]').value;
+  const password = event.target.querySelector('input[name="password"]').value;
+  const data = {
+    name,
+    email,
+    password,
+  };
+  const url = `${API_URL}/signup`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json())
+    .then((resData) => {
+      const { message, name, email } = resData;
+      if (message) {
+        alert('Во время запроса произошла ошибка');
+      } else {
+        alert('Переключиться на Sign In');
+      }
+      console.log(resData);
+    })
+    .catch((err) => {
+      alert('Во время запроса произошла ошибка');
+      console.log(err);
+    });
+});
 
 headerLinkRounded.addEventListener('click', () => {
   popup.classList.add('popup_is-opened');
